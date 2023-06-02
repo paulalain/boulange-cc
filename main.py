542efa701fae336370fcf61087ed22a5e0c83198
Paul-Alain Bugnard <paul-alain.bugnard@yahooinc.com>
2023-06-02 20:59:11 +0200

    add timeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from api.pois import get_pois, check_type
-from utils.gpx_upload import upload_gpx_file, delete_gpx_file
+from utils.gpx_upload import upload_gpx_file, delete_gpx_file, delete_cache
 from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
@@ -9,14 +9,16 @@ app.secret_key = "super secret key"
 @app.route('/<poi_type>', methods = ['POST'])
 def poi_type(poi_type):
     file_path = upload_gpx_file(request.files)
+    pois = ''
 
     if check_type(poi_type):
-        return get_pois(poi_type, file_path)
+        pois = get_pois(poi_type, file_path)
     
     # Remove file
     delete_gpx_file(file_path)
+    delete_cache()
 
-    return None
+    return pois
 
 @app.route('/')
 def index():
--- utils/gpx_upload.py
+++ utils/gpx_upload.py
@@ -1,5 +1,5 @@
 from flask import flash
-import os
+import os, shutil
 import uuid
 from werkzeug.exceptions import BadRequest, UnsupportedMediaType
 
@@ -42,3 +42,15 @@ def upload_gpx_file(files):
 def delete_gpx_file(filename):
     if os.path.exists(filename):
         os.remove(filename)
+
+def delete_cache():
+    folder = 'cache'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
